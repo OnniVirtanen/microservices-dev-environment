d2726159b05c74b9a746fad904b5bbe82d968746
Onni Virtanen <onni.virtanen5@gmail.com>
2024-06-20 23:55:21 +0300

    cleaned code in shipping service

--- shipping-service/src/main/java/com/virtanen/shipping/demo/domain/ShippingService.java
+++ shipping-service/src/main/java/com/virtanen/shipping/demo/domain/ShippingService.java
@@ -1,9 +1,11 @@
 package com.virtanen.shipping.demo.domain;
 
 import com.virtanen.shipping.demo.domain.model.InformShipmentEvent;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ShippingService {
 
+    @Transactional(readOnly = false)
     void handleShipment(InformShipmentEvent event);
 
 }
