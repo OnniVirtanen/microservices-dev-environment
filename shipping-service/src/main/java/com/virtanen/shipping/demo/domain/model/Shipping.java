e1840eeb68f8a6cc8c20a39407f135be78f32d44
Onni Virtanen <onni.virtanen5@gmail.com>
2024-06-29 13:49:32 +0300

    shipping service event

--- shipping-service/src/main/java/com/virtanen/shipping/demo/domain/model/Shipping.java
+++ shipping-service/src/main/java/com/virtanen/shipping/demo/domain/model/Shipping.java
@@ -1,5 +1,6 @@
 package com.virtanen.shipping.demo.domain.model;
 
+import com.virtanen.shipping.demo.domain.event.dto.Address;
 import org.springframework.data.annotation.Id;
 
 public class Shipping {
